349454a7fcabe18000bb24202d5178d9795bd0fe
Faria-Lorin <florin20@stuy.edu>
2020-10-30 00:08:13 -0400

    I wrote code for teh method equals which returns true when the x and y vaules of the two points are the same

--- Point.java
+++ Point.java
@@ -20,8 +20,15 @@ public class Point{
  }
 
  public double distanceTo(Point other){
-   double dist = Math.sqrt(Math.pow((x-other.x), 2) + Math.pow((y-other.y), 2));
+   double dist = Math.sqrt(Math.pow((this.x-other.x), 2) + Math.pow((this.y-other.y), 2));
    System.out.println(dist);
    return dist;
+ }
+
+ public boolean equals( Point other){
+   if ((this.x == other.x) && (this.y == other.y)){
+     return true;
+   }
+   return false;
  }
 }
